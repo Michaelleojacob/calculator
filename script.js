a90817b1a2192f80eb5a14a1eae6ec26355f0c18
Michael <michaelleojacob@gmail.com>
2021-03-07 17:04:45 -0800

    I think i'm at a good point? I put the display to show the arr. I can currently add 9, and only 9 lol. I would like to add logic so that all the values are input correctly. So a user clicks on 3, the javascript will read a value of 3, instead of me creating a if statement for if e.target.className === 3, push(3) bs.

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-let arr = []
+let arr = [123]
+const display = document.querySelector(".resultdisplay");
+display.textContent = arr;
+const buttons = document.querySelectorAll('.buttons');
+
+for(let i=0; i<buttons.length; i++){
+    buttons[i].addEventListener('click', function(e){
+        console.log(e.target.className);
+        console.log(e);
+        console.log(this)
+
+        if(e.target.className === "buttons nine"){
+            console.log("nine")
+            arr.push(9);
+            display.textContent = arr;
+        }
+
+    } );
+}
+
 
 const operate = function(a, operater, b){
 
